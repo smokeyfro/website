e8b271777e07f5e5b489096b4a6c7d1c650ecd66
Chris Rault <chris@smokeyfro.com>
2023-08-07 12:04:17 +0200

    Fixed the inner layout padding

--- components/layout/main.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <main class="bg:rgba(255,255,255,0.85)">
-        <div class="font:20 overflow:hidden h:full">
-            <perfect-scrollbar>
-                <slot />
-            </perfect-scrollbar>
-        </div>
-    </main>
-</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -8,7 +8,9 @@
     <main class="bg:rgba(255,255,255,0.85)">
       <div class="font:20 overflow:hidden h:full">
         <perfect-scrollbar>
-          <slot />
+          <div class="p:80">
+            <slot />
+          </div>
         </perfect-scrollbar>
       </div>
     </main>
