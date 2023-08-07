ef286b20b5774fce5317c7ccc323407ee14c354d
Chris Rault <chris@smokeyfro.com>
2023-08-07 16:31:15 +0200

    Trigger rebuild

--- pages/contact.vue
+++ pages/contact.vue
@@ -5,5 +5,8 @@
     </template>
     <base-title>Contact</base-title>
     <base-excerpt>Got a project you would like to discuss? Or maybe you just want to say hello? Fill out the form below and I'll do my best to respond asap.</base-excerpt>
+    <form>
+
+    </form>
   </nuxt-layout>
 </template>
