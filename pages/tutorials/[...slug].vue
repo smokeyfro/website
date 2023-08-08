ee519fcef3c7187ef4a43a0a9f856c45c2f9507a
Chris Rault <chris@smokeyfro.com>
2023-08-08 17:01:09 +0200

    Testing with query

--- pages/tutorials/[...slug].vue
+++ pages/tutorials/[...slug].vue
@@ -4,13 +4,17 @@
       <base-nav :subnav="NavLinks[4].subnav" />
     </template>
     <div class="max-width:760">
-      <div class="text:xl ">
-        <ContentDoc v-slot="{ doc }">
-          <base-title>{{ doc.title }}</base-title>
-          <base-excerpt>{{ doc.excerpt }}</base-excerpt>
-          <ContentRenderer :value="doc" />
-        </ContentDoc>
-      </div>
+      <ContentDoc v-slot="{ doc }">
+        <base-title>{{ doc.title }}</base-title>
+        <base-excerpt>{{ doc.excerpt }}</base-excerpt>
+        <ContentRenderer :value="doc" />
+      </ContentDoc>
     </div>
   </nuxt-layout>
 </template>
+<script setup lang="ts">
+  const { path } = useRoute()
+  const { data: doc } = await useAsyncData(`content-${path}`, () => {
+    return queryContent().where({ _path: path }).findOne();
+  });
+</script>
