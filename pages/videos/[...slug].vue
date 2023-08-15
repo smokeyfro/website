d449247a82d0f9272fa96d9511e5c6016497c35f
Chris Rault <chris@smokeyfro.com>
2023-08-15 08:17:12 +0200

    Rebuild

--- pages/videos/[...slug].vue
+++ pages/videos/[...slug].vue
@@ -3,13 +3,13 @@
     <template v-slot:aside>
       <base-nav :subnav="NavLinks[1].subnav" />
     </template>
-    <base-title>{{ video.title }}</base-title>
-    <base-excerpt>{{ video.excerpt }}</base-excerpt>
-    <div class="w:full h:auto">
+    <div v-if="page">
+      <base-title>{{ page.title }}</base-title>
+      <base-excerpt>{{ page.excerpt }}</base-excerpt>
       <lite-youtube
-        :videoid="video.video_id"
-        :playlabel="video.title"
-        :start="video.start ? video.start : 0"
+        :videoid="page.video_id"
+        :playlabel="page.title"
+        :start="page.start ? page.start : 0"
         params="controls=0&modestbranding=2&rel=0&enablejsapi=1"
         class="video"
       />
@@ -25,7 +25,7 @@
 <script setup lang="ts">
   import type { Video } from '~/types'
   const { path } = useRoute()
-  const { data: video } = await useAsyncData(path.replace(/\/$/, ''),
+  const { data: page } = await useAsyncData(path.replace(/\/$/, ''),
     () => queryContent<Video>('videos')
       .where({ _path: path })
       .findOne(),
