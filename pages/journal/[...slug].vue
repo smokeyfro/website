c616d755940f2656da0604454e9310956cce75d2
Chris Rault <chris@smokeyfro.com>
2023-08-15 09:08:23 +0200

    Rebuild

--- pages/journal/[...slug].vue
+++ pages/journal/[...slug].vue
@@ -21,81 +21,9 @@
 <script setup lang="ts">
   import type { Post } from '~/types'
   const { path } = useRoute()
-
-const { data: page } = await useAsyncData(path.replace(/\/$/, ''),
-  () => queryContent<Post>('journal')
-    .where({ _path: path })
-    .findOne(),
-)
-  const title: string = page.value?.title || ''
-  const description: string = page.value?.excerpt || ''
-  const image: string = page.value?.image || ''
-  const ogImage: string = page.value?.image || ''
-  useHead({
-    title: page.value?.title || '',
-    meta: [
-      { 
-        name: 'description', 
-        content: description },
-      {
-        name: 'description',
-        content: description,
-      },
-      { 
-        property: 'og:site_name', 
-        content: 'SmokeyFro' 
-      },
-      { 
-        hid: 'og:type', 
-        property: 'og:type', 
-        content: 'website' 
-      },
-      {
-        property: 'og:url',
-        content: 'https://smokeyfro.com',
-      },
-      {
-        property: 'og:title',
-        content: title,
-      },
-      {
-        property: 'og:description',
-        content: description,
-      },
-      {
-        property: 'og:image',
-        content: ogImage || image,
-      },
-      { 
-        name: 'twitter:site', 
-        content: '@smokeyfro' 
-      },
-      { 
-        name: 'twitter:card', 
-        content: 'summary_large_image' 
-      },
-      {
-        name: 'twitter:url',
-        content: 'https://smokeyfro.com',
-      },
-      {
-        name: 'twitter:title',
-        content: title,
-      },
-      {
-        name: 'twitter:description',
-        content: description,
-      },
-      {
-        name: 'twitter:image',
-        content: ogImage || image,
-      },
-    ],
-    link: [
-      {
-        rel: 'canonical',
-        href: `https://smokeyfro.com${path}`,
-      },
-    ],
-  })
+  const { data: page } = await useAsyncData(path.replace(/\/$/, ''),
+    () => queryContent<Post>('journal')
+      .where({ _path: path })
+      .findOne(),
+  )
 </script>
