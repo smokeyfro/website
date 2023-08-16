2541f7d867c31dbd17fab02d4d22ed705976cf20
Chris Rault <chris@smokeyfro.com>
2023-08-16 10:20:00 +0200

    Rebuild

--- pages/tutorials/topics/[slug].vue
+++ pages/tutorials/topics/[slug].vue
@@ -29,10 +29,9 @@ useHead({
     <template v-slot:aside>
       <base-nav :subnav="NavLinks[4].subnav" />
     </template>
-    <div class="max-width:960">
     <base-title>{{ title }}</base-title>
     <base-excerpt>{{ description }}</base-excerpt>
-    <topics-cloud :section="section" />
+    <topics-cloud />
     <ul class="list-style:none m:0 mt:40 p:0 flex flex:col gap:40">
       <li v-for="{ _path: slug, title, thumb, topic, tags } in tutorials" :key="slug">
         <nuxt-link :to="slug" class="text-decoration:none color:#222 bg:#fff {bg:#f7f7f7}:hover ~all|300ms|ease-in-out r:5 overflow:hidden flex ai:center b:1|solid|#ccc">
@@ -40,10 +39,10 @@ useHead({
           <div class="py:40 px:55">
             <h2 class="m:0">{{ title }}</h2>
             <div class="flex ai:center gap:10 mt:25 mb:10">
-              <span class="px:6 py:2 f:14 r:3"
+              <span class="px:6 py:2 f:14 r:3 color:#fff"
                 :class="{
-                'bg:pink color:#fff': topic === 'Going Headless',
-                'bg:green color:#fff': topic === 'Learning Vue',
+                'bg:pink': topic === 'Going Headless',
+                'bg:green': topic === 'Learning Vue',
               }">{{ topic }}</span>
               &middot;
               <span v-if="tags && tags.length" class="flex ai:center jc:start gap:15">
@@ -55,6 +54,5 @@ useHead({
         </nuxt-link>
       </li>
     </ul>
-    </div>
   </nuxt-layout>
 </template>
